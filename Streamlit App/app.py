30bd07b930e79c94385d9086bdf03008689d3ee8
okodun <felix@upndown.de>
2022-04-30 11:38:49 +0200

    Created Streamlit app

--- Streamlit App/app.py
+++ Streamlit App/app.py
@@ -1,6 +1,20 @@
+"""
+Created by Felix Schuhmann, Hussein Gallal, Philippe Huber
+favicon created by: https://favicon.io/emoji-favicons/lungs
+"""
+
 import streamlit as st
-import streamlit_image_comparison as image_comparison
 
-st.set_page_config(page_title="Test", layout="centered")
+# texts and descriptions
+ABOUT_TEXT = "Created by Felix Schuhmann, Hussein Gallal and Philippe Huber."
+ICON = "/home/felix/PycharmProjects/Chest-X-Ray-Classification/Streamlit App/.images/lungs_favicon.png"
 
-image_comparison(image1="", image2="")
+# set page configuration for app
+st.set_page_config(page_title="Chest X-Ray Classification",
+                   page_icon=ICON,
+                   layout="wide",
+                   menu_items={"About": ABOUT_TEXT})
+
+# set title and header for introduction
+st.title("Chest X-Ray Classification")
+st.header("A project by group 3")
