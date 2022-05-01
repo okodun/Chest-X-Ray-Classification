9fa9754386bdbdb7a4e6c3ba33b7ab6ff2bb0acd
okodun <felix@upndown.de>
2022-05-01 17:06:42 +0200

    Version 0.9

--- Streamlit App/app.py
+++ Streamlit App/app.py
@@ -22,14 +22,24 @@ st.set_page_config(page_title="Chest X-Ray Classification",
 st.title("Chest X-Ray Classification")
 st.header("Pneumonia - A short introduction")
 pneumonia_text = """
-Pneumonia is a respiratory infection caused by bacteria, viruses or fungi.
-Did you know that pneumonia accounts for approximately 14% of all deaths of children under 5 years old? [1]
-However, the disease can be dangerous for adults as well... 
+Pneumonia is a respiratory infection caused by bacteria, viruses or fungi. Did you know that pneumonia accounts
+for approximately 14% of all deaths of children under 5 years old [1]? However, the disease can be dangerous for
+adults as well [2]. Chest x-ray is currently the best way to reliably detect pneumonia. But the diagnosis relies
+on the trained eye of a radiologist [3]. 
 """
 st.text(pneumonia_text)
 
 # comparison of lungs
 image_comparison(img1=HEALTHY_LUNG, label1="A Healthy Lung", img2=INFECTED_LUNG, label2="Lung With Pneumonia")
+
+# what and how are we going to solve this
+st.header("The Project")
+project_description = """
+We use machine learning to analyse chest x-ray images.Our goal is to implement an app which can ultimately detect
+pneumonia and help physicians to reliably diagnose pneumonia. Just kidding, we are not trying to change the world
+(yet). We are doing this project as part of our seminar ML4B.
+"""
+st.text(project_description)
 
 # introduction team members
 st.header("The Team")
@@ -63,5 +73,11 @@ with st.expander("References"):
     references = """
     [1] Pneumonia, World Health Organization
     https://www.who.int/news-room/fact-sheets/detail/pneumonia
+    
+    [2] Five Facts You Should Know About Pneumonia, American Lung Association
+    https://www.lung.org/lung-health-diseases/lung-disease-lookup/pneumonia/five-facts-you-should-know
+    
+    [3] CheXNet: Radiologist-Level Pneumonia Detection on Chest X-Rays with Deep Learning, Rajpurkar et al. 2017
+    https://arxiv.org/pdf/1711.05225.pdf
     """
     st.text(references)
