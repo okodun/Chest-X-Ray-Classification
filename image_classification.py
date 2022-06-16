5b43cb470137586fcb99412edd38e8320239f0ad
okodun <felix@upndown.de>
2022-06-16 16:04:42 +0200

    Basic Keras Model

--- image_classification.py
+++ image_classification.py
@@ -1,11 +1,13 @@
 """
 https://www.projectpro.io/article/deep-learning-for-image-classification-in-python-with-cnn/418#toc-1
+(https://towardsdatascience.com/medical-x-ray-%EF%B8%8F-image-classification-using-convolutional-neural-network-9a6d33b1c2a)
 """
 
 import tensorflow as tf
 from pathlib import Path
 import pandas
 import matplotlib.pyplot as plt
+import cv2
 
 BASE_PATH = "/home/felix/Documents/University/SS2022/ML4B/Data Set Chest X-Ray"
 
@@ -16,8 +18,33 @@ def __check_cuda_gpu() -> bool:
     return tf.test.is_gpu_available(cuda_only=True)
 
 
-def load_xrays(base_path: str, mode: str) -> pandas.DataFrame:
-    """ loads images as pandas DataFrame for either training, testing or valuation """
+def __plot(data: [], labels: []):
+    """ plots given data with labels """
+
+    df = pandas.DataFrame(data)
+    df.columns = ['images']
+    df['labels'] = labels
+    df = df.sample(frac=1).reset_index(drop=True)
+    plt.bar(df['labels'].value_counts().index, df['labels'].value_counts().values)
+    plt.show()
+
+
+def __show_sample_images(data: [], labels: []):
+    """ shows first 10 images in data (data contains only the paths to these images) """
+
+    plt.figure(figsize=(10, 5))
+    for i in range(0, 10):
+        plt.subplot(2, 5, i + 1)
+        img = cv2.imread(str(data[i]))
+        img = cv2.resize(img, (512, 512))
+        plt.imshow(img)
+        plt.title(labels[i])
+        plt.axis("off")
+    plt.show()
+
+
+def get_data(base_path: str, mode: str) -> ():
+    """ loads images as pandas DataFrame for either training, testing or validation """
 
     # raise exception if mode is unknown
     if mode not in ("test", "train", "val"):
@@ -32,25 +59,20 @@ def load_xrays(base_path: str, mode: str) -> pandas.DataFrame:
     train_labels = []
     # normal cases
     normal_dir = wd / "NORMAL"
-    normal = normal_dir.glob("*.jpeg")
-    for n in normal:
+    normal_images = normal_dir.glob("*.jpeg")
+    for n in normal_images:
         train_data.append(n)
         train_labels.append("NORMAL")
     # unhealthy cases
     pneumonia_dir = wd / "PNEUMONIA"
-    pneumonia = pneumonia_dir.glob("*.jpeg")
-    for p in pneumonia:
+    pneumonia_images = pneumonia_dir.glob("*.jpeg")
+    for p in pneumonia_images:
         train_data.append(p)
         train_labels.append("PNEUMONIA")
 
-    # plot number of images
-    df = pandas.DataFrame(train_data)
-    df.columns = ['images']
-    df['labels'] = train_labels
-    df = df.sample(frac=1).reset_index(drop=True)
-    return df
+    return train_data, train_labels
 
 
-test = load_xrays(BASE_PATH, "val")
-plt.bar(test['labels'].value_counts().index, test['labels'].value_counts().values)
-plt.show()
+train = get_data(BASE_PATH, "val")
+__plot(train[0], train[1])
+__show_sample_images(train[0], train[1])
