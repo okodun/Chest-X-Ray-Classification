82d653a0d61afc7e6aee57d327d5da44d99231ea
okodun <felix@upndown.de>
2022-06-18 23:03:50 +0200

    Implemented first model

--- image_classification.py
+++ image_classification.py
@@ -6,14 +6,14 @@ https://keras.io/examples/vision/image_classification_from_scratch/
 
 import tensorflow as tf
 import keras
-from keras import layers, utils
+from keras import layers, utils, callbacks, optimizers
 from pathlib import Path
 import pandas
 import matplotlib.pyplot as plt
 import cv2
 
 BASE_PATH = "/home/felix/Documents/University/SS2022/ML4B/Data Set Chest X-Ray"
-IMAGE_SIZE = (512, 512)
+IMAGE_SIZE = (256, 256)
 BATCH_SIZE = 16
 
 
@@ -106,23 +106,100 @@ def create_dataset(mode: str):
     )
 
 
-""" create model """
+def create_model(input_shape, num_classes):
+    """ create model """
 
-# data augmentation
-data_augmentation = keras.Sequential([layers.RandomFlip("horizontal"), layers.RandomRotation(0.1)])
+    # define parameters and augment data
+    inputs = keras.Input(shape=input_shape)
+    data_augmentation = keras.Sequential([layers.RandomFlip("horizontal"), layers.RandomRotation(0.1)])
+    x = data_augmentation(inputs)
 
-train_ds = create_dataset("train")
-# val_ds = create_dataset("val")
-# test_ds = create_dataset("test")
+    x = layers.Rescaling(1.0 / 255)(x)
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(64, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+    return keras.Model(inputs, outputs)
 
 
-plt.figure(figsize=(10, 10))
-for images, _ in train_ds.take(1):
-    for i in range(9):
-        augmented_images = data_augmentation(images)
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(augmented_images[0].numpy().astype("uint8"))
-        plt.axis("off")
+def train_model():
+    model = create_model(input_shape=IMAGE_SIZE + (3,), num_classes=2)
+    # keras.utils.plot_model(model, show_shapes=True)
+    train_ds = create_dataset("train")
+    val_ds = create_dataset("val")
 
-plt.show()
-# save model!!!
+    epochs = 50
+
+    callbacks = [
+        keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+    ]
+    model.compile(
+        optimizer=keras.optimizers.Adam(1e-3),
+        loss="binary_crossentropy",
+        metrics=["accuracy"],
+    )
+    model.fit(
+        train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
+    )
+
+
+val_ds = create_dataset("val")
+model = keras.models.load_model("save_at_50.h5")
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-3),
+    loss="binary_crossentropy",
+    metrics=["accuracy"],
+)
+print(model.evaluate(val_ds))
+
+"""img_path = "/home/felix/Documents/University/SS2022/ML4B/Data Set Chest X-Ray/val/NORMAL/NORMAL2-IM-1431-0001.jpeg"
+img = keras.utils.load_img(
+    img_path, target_size=IMAGE_SIZE
+)
+img_array = keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+predictions = model.predict(x=img_array)
+score = predictions[0]
+print(
+    "This image is %.2f percent normal and %.2f percent pneumonia."
+    % (100 * (1 - score), 100 * score)
+)"""
