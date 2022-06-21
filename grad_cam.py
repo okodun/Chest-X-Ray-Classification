dce381546b7066e93f9b78f503a5215f69a9aa43
okodun <felix@upndown.de>
2022-06-21 19:30:31 +0200

    GradCam completed

--- grad_cam.py
+++ grad_cam.py
@@ -2,18 +2,11 @@
 please note: this class is almost 100% from https://keras.io/examples/vision/grad_cam/ (we implemented minor changes)
 """
 
-import numpy as np
 import tensorflow as tf
-from keras import utils
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import keras
-
-# constants
-IMAGE_SIZE = (256, 256)
-LAST_CONV_LAYER_NAME = "separable_conv2d_8"
-IMAGE_PATH = "/home/felix/Documents/University/SS2022/ML4B/Data Set Chest X-Ray/test/NORMAL/IM-0011-0001-0002.jpeg"
-MODEL_PATH = "save_at_50.h5"
+from keras import utils
+import matplotlib.cm as cm
+import numpy as np
 
 
 class GradCam(object):
@@ -37,30 +30,25 @@ class GradCam(object):
         config = self.MODEL.get_config()["layers"][0]["config"]["batch_input_shape"]
         self.IMAGE_SIZE = config[1], config[2]
 
-    @staticmethod
-    def __load_image_array(image_path: str, image_size: ()):
-        """ loads an image as an array with additional batch """
-
-        image = keras.utils.load_img(image_path, target_size=image_size)
-        image_array = keras.utils.img_to_array(image)
-        return np.expand_dims(image_array, axis=0)
-
-    def __create_heatmap(img_path: str, model_path: str, last_conv_layer_name, pred_index=None):
+    def create(self, path_to_image: str, pred_index=None, cam_path="grad_cam.jpg", alpha=0.4):
         """ creates a heatmap and determines the decision criteria of a trained model """
 
         # create model with that maps input to output
-        model = keras.models.load_model(model_path)
+        model = self.MODEL
         grad_model = tf.keras.models.Model(
-            [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+            [model.inputs], [model.get_layer(self.LAST_CONVOLUTIONAL_LAYER).output, model.output]
         )
 
         # load image as array
-        img_array = GradCam.__load_image_array(img_path, IMAGE_SIZE)
+        # img_array = GradCam.__load_image_array(path_to_image, self.IMAGE_SIZE)
+        image = keras.utils.load_img(path_to_image, target_size=self.IMAGE_SIZE)
+        image_array = keras.utils.img_to_array(image)
+        image_array = np.expand_dims(image_array, axis=0)
 
         # Then, we compute the gradient of the top predicted class for our input image
         # with respect to the activations of the last conv layer
         with tf.GradientTape() as tape:
-            last_conv_layer_output, preds = grad_model(img_array)
+            last_conv_layer_output, preds = grad_model(image_array)
             if pred_index is None:
                 pred_index = tf.argmax(preds[0])
             class_channel = preds[:, pred_index]
@@ -82,11 +70,10 @@ class GradCam(object):
 
         # For visualization purpose, we will also normalize the heatmap between 0 & 1
         heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
-        return heatmap.numpy()
+        heatmap = heatmap.numpy()
 
-    def __save_and_display_gradcam(img_path, heatmap, cam_path="cam2.jpg", alpha=0.4):
         # Load the original image
-        img = keras.utils.load_img(img_path)
+        img = keras.utils.load_img(path_to_image)
         img = keras.utils.img_to_array(img)
 
         # Rescale heatmap to a range 0-255
@@ -110,35 +97,3 @@ class GradCam(object):
 
         # Save the superimposed image
         superimposed_img.save(cam_path)
-
-
-def show(image_path: str, image_size: ()):
-    """ plots and shows image """
-
-    image = keras.utils.load_img(image_path, target_size=image_size)
-    image_array = keras.utils.img_to_array(image)
-    plt.imshow(image_array.astype("uint8"))
-    plt.show()
-
-
-"""
-if __name__ == '__main__':
-    # prepare image
-    img_array = __load_image_array(IMAGE_PATH, IMAGE_SIZE)
-
-    # make model
-    model = keras.models.load_model("save_at_50.h5")
-    g = GradCam(model)
-
-    # Print what the top predicted class is
-    preds = model.predict(x=img_array)
-    print("PNEUMONIA: %.2f" % (100 * preds[0]))
-
-    # Remove last layer's activation function
-    model.layers[-1].activation = None
-
-    # Generate class activation heatmap
-    heatmap = create_heatmap(IMAGE_PATH, MODEL_PATH, LAST_CONV_LAYER_NAME)
-
-    # save heatmap
-    # save_and_display_gradcam(IMAGE_PATH, heatmap)"""
