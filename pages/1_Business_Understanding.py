5544de3490e843b61253d4c6096be9fc610f841e
AbderrahmaneBennani <93011714+AbderrahmaneBennani@users.noreply.github.com>
2022-07-06 07:31:15 +0200

    BU.1

--- pages/1_Business_Understanding.py
+++ pages/1_Business_Understanding.py
@@ -1,9 +1,27 @@
 import streamlit as st
 
 st.title("Business Understanding")
-st.header("Header")
-st.subheader("Subheader")
-block1="1- Business Objectives"
-block2="2- Asses Situation"
-block3="3- Determine Data Mining goals"
-block4="4- Produce Project Plan"
+
+# Blocks of texts
+Description = "The initial Business Understanding phase focuses on understanding the project objectives and " \
+              "requirements from a business perspective, and then converting this knowledge into a data mining " \
+              "problem definition, and a preliminary project plan designed to achieve the objectives "
+block1 = "1- Business Objectives"
+block2 = "2- Asses Situation"
+block3 = "3- Determine Data Mining goals"
+block4 = "4- Produce Project Plan"
+
+st.header("Definition")
+st.text(Description)
+st.header("1- Business Objectives")
+st.text(block1)
+st.header("2- Asses Situation")
+st.text(block2)
+st.header("3- Determine Data Mining goals")
+st.text(block3)
+st.header("4- Produce Project Plan")
+st.text(block4)
+
+# TODO Fill all blocks
+# TODO Add Images
+# st.subheader("Subheader")
