3730bf65561a6fa7bb5538dc10aec4f973207681
AbderrahmaneBennani <93011714+AbderrahmaneBennani@users.noreply.github.com>
2022-07-06 07:06:21 +0200

    testing

--- pages/1_Business_Understanding.py
+++ pages/1_Business_Understanding.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("Business Understanding")
+st.header("Header")
+st.subheader("Subheader")
+block1="1- Business Objectives"
+block2="2- Asses Situation"
+block3="3- Determine Data Mining goals"
+block4="4- Produce Project Plan"
