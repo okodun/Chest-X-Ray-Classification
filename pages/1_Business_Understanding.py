70c644343bcf730ac8636d95e21bb4f2494f0fcf
AbderrahmaneBennani <93011714+AbderrahmaneBennani@users.noreply.github.com>
2022-07-07 13:34:06 +0200

    BU

--- pages/1_Business_Understanding.py
+++ pages/1_Business_Understanding.py
@@ -15,19 +15,22 @@ Description = """The initial Business Understanding phase focuses on understandi
 objectives and requirements from a business perspective, and then converting 
 this knowledge into a data mining problem definition, and a preliminary
 project plan designed to achieve the objectives """
-block1 = """- Creating a machine learning model that can accurately indentify pneumonia infected lungs+ 
-- Business success criteria: Accuracy, consistency..."""
-block2 = """Inventory of Ressources: 
-+ Requirements, Assumptions and Constraints 
-+ Risks and Contingencies"""
-block3 = """Determine Data Mining Goals 
-+ Data Mining Success Criteria"""
+block1 = """- Creating a machine learning model that will help facilitate identifying pneumonia in
+infected lungs. 
+- Business success criteria: The Model has to be superior to normal diagnosis made by doctors"""
+block2 = """Inventory of Ressources:
++ Hardware: NVIDIA RTX 2060 SUPER (6GB dedicated RAM) / AMD Ryzen 3900x
++ Software: Kaggle, GradCam, Keras, 
++ Dataset: Chest X-Ray Images (Pneumonia)"""
+block3 = """Data Mining Goal: A model that determines from a Chest X-Ray if the patient
+has Pneumonia or not.
+Data Mining Success Criteria: The Model has to be accurate and consistent"""
 block4 = """Project Plan"""
 image1 = """Pictures/CRISP-DM_Business_understanding.png"""
 
 st.header("Definition")
-st.image(image1)
 st.text(Description)
+st.image(image1)
 
 st.header("1- Business Objectives")
 st.text(block1)
